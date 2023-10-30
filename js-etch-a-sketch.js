cd3f7fa119a96069ecb0d210ead28064d1080292
Kiff <125292279+kiffoh@users.noreply.github.com>
2023-10-30 12:12:17 -0500

    Utilise user input to alter the number of squares

--- js-etch-a-sketch.js
+++ js-etch-a-sketch.js
@@ -1,34 +1,49 @@
+const btn = document.createElement("button");
+btn.classList.add("gridsize")
+document.body.appendChild(btn)
+
+btn.addEventListener("click", function() {
+    let squares = prompt("How many squares would you like each side of the container to have? (max: 100)");
+    createDivs(squares)
+})
+
+
+
 const container = document.createElement("div")
 container.classList.add("container")
 document.body.appendChild(container)
 
-container.style.width = "400px"; 
-container.style.height = "400px";
-container.style.display = "flex";
-container.style.flexWrap = "wrap";
-
-// container.style.position = "relative";
+container.style.width = "500px"; 
+container.style.height = "500px";
+container.style.position = "relative";
 
 
-for (let i=0; i < 4; i++) {
+function createDivs(m=4) {
+    for (let i=0; i < m; i++) {
 
-    
-    for (let j = 0; j < 4; j ++) {
-        const childDiv = document.createElement("div");
-        childDiv.classList.add("child");
-        container.appendChild(childDiv);
-
-        // Set the position, coordinates and styles of the child divs
-        childDiv.style.position = "absolute";
-        childDiv.style.top = `${i/4 * container.offsetHeight}px`; // Changed container.height to container.offsetHeight
-        childDiv.style.left = `${j/4 * container.offsetWidth}px`; // Changed right to left
         
+        for (let j = 0; j < m; j ++) {
+            const childDiv = document.createElement("div");
+            childDiv.classList.add("child");
+            container.appendChild(childDiv);
 
-        childDiv.style.width = `${container.offsetHeight/4}px`;
-        childDiv.style.height = `${container.offsetWidth/4}px`;    
-        childDiv.style.backgroundColor = "pink"
+            // Set the position, coordinates and styles of the child divs
+            childDiv.style.position = "absolute";
+            childDiv.style.top = `${i/m * container.offsetHeight}px`; // Changed container.height to container.offsetHeight
+            childDiv.style.left = `${j/m * container.offsetWidth}px`; // Changed right to left
+            
+
+            childDiv.style.width = `${container.offsetHeight/m}px`;
+            childDiv.style.height = `${container.offsetWidth/m}px`;    
+            childDiv.style.backgroundColor = "pink"
+
+            childDiv.addEventListener("mouseover", function() {
+                childDiv.style.backgroundColor = "blue"
+            })
+            childDiv.addEventListener("mouseout", function() {
+                childDiv.style.backgroundColor = "pink"
+            })
+        }
+
     }
-
-    
-
 }
